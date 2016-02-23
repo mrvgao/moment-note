75f531d37b16b3eb62b88ba503e1d3f38446c82f
fortyMiles <mqgao@outlook.com>
2016-02-23 18:47:51 +0800

    change mutable to True

--- wheat/apps/user/apis.py
+++ wheat/apps/user/apis.py
@@ -129,10 +129,10 @@ class UserViewSet(ListModelMixin,
               paramType: body
         '''
 
-#        request.POST._mutable = True
+        request.POST._mutable = True
 #        post_data = request.data.copy()
-        phone = request.data.get('phone', None)
-        password = request.data.get('password', None)
+        phone = request.data.pop('phone', None)
+        password = request.data.pop('password', None)
         if not phone or not password:
             return SimpleResponse(status=status.HTTP_400_BAD_REQUEST)
         if not utils.valid_phone(phone) or not utils.valid_password(password):
@@ -140,7 +140,7 @@ class UserViewSet(ListModelMixin,
         user = UserService.get_user(phone=phone)
         if user:
             return SimpleResponse(status=status.HTTP_409_CONFLICT)
-        user = UserService.create_user(**request.data)
+        user = UserService.create_user(phone=phone, password=password, **request.data)
         data = UserService.serialize(user)
         UserService.login_user(request, phone, password)
         return SimpleResponse(data)
@@ -337,7 +337,7 @@ class UserViewSet(ListModelMixin,
             'phone': phone,
             'captcha': code
         }
-        
+
         if not send_succeed:
             return SimpleResponse(success=False)
         else:

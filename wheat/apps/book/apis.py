b561510488b99658f558bff022646087b252af4e
fortyMiles <mqgao@outlook.com>
2016-02-26 18:42:13 +0800

    add url comment

--- wheat/apps/book/apis.py
+++ wheat/apps/book/apis.py
@@ -20,9 +20,32 @@ class AuthorViewSet(ListModelMixin, viewsets.GenericViewSet):
     Or(permissions.IsAuthenticatedOrReadOnly, AllowPostPermission,)]
 
     def list(self, request):
+        '''
+        获得关于作者的信息，根据参数的不同可以获得单个user的和user_group的信息
+
+        ### Request Example:
+
+        /author/type=person&id=asdhjk21hjkads2134
+
+        获取用户id为XXX的个人信息
+
+        /author/type=group&id=asdhjk21hjkads2134
+
+        获取作者群组id为XXX的群组信息
+
+        type -- 查询类型 (person | group)
+        id -- 用户或者group的id (String)
+
+        ---
+
+        '''
         pass
 
     def create(self, request):
+        '''
+        根据user_id列表生成一个“作者群”
+
+        '''
         pass
 
 
@@ -35,12 +58,23 @@ class BookViewSet(ListModelMixin, viewsets.GenericViewSet):
     Or(permissions.IsAuthenticatedOrReadOnly, AllowPostPermission,)]
 
     def list(self, request):
+        '''
+        获得关于书本的信息
+        ---
+        id -- Book id
+        '''
         pass
 
     def create(self, request):
+        '''
+        创建书籍信息
+        '''
         pass
 
     def update(self, request, book_id):
+        '''
+        更新书籍信息
+        '''
         pass
 
 
@@ -53,10 +87,16 @@ class OrderViewSet(ListModelMixin, viewsets.GenericViewSet):
     Or(permissions.IsAuthenticatedOrReadOnly, AllowPostPermission,)]
     
     def list(self, reqeust):
+        '''
+        '''
         pass
 
     def create(self, request):
+        '''
+        '''
         pass
 
     def update(self, request, order_id):
+        '''
+        '''
         pass

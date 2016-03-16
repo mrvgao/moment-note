a515cd1a2d0121c7dc6d4720cba3fce4411ee371
fortyMiles <mqgao@outlook.com>
2016-03-16 17:40:52 +0800

    modified start_gunicorn_server.sh: add command line options

--- wheat/start_gunicorn_server.sh
+++ wheat/start_gunicorn_server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
  
+# default configuration
 NAME="wheat-api"                                  # Name of the application
 DJANGODIR=/workspace/wheat-api/wheat            # Django project directory
 PORT=8484
@@ -7,11 +8,58 @@ USER=develop                                       # the user to run as
 GROUP=develop # the group to run as
 NUM_WORKERS=4                                     # how many worker processes should Gunicorn spawn
 TIME_OUT=90
-DJANGO_SETTINGS_MODULE=settings             # which settings file should Django use
 DJANGO_WSGI_MODULE=wsgi                     # WSGI module name
-GUNICORN_ACCESS_LOG=/workspace/wheat-api/wheat/logs/gunicorn-access.log
-GUNICORN_ERROR_LOG=/workspace/wheat-api/wheat/logs/gunicorn-error.log
- 
+
+
+###### Get command line options ###### 
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+while getopts "h?n:d:p:u:g:c:w:t:" opt; do
+    case "$opt" in
+    h|\?)
+        echo "help..."
+        exit 0
+        ;;
+    n)  NAME=$OPTARG
+        ;;
+    d)  DJANGODIR=$OPTARG
+        ;;
+    p)  PORT=$OPTARG
+        ;;
+    u)  USER=$OPTARG
+        ;;
+    g)  GROUP=$OPTARG
+        ;;
+    c)  NUM_WORKERS=$OPTARG
+        ;;
+    w)  DJANGO_WSGI_MODULE=$OPTARG
+        ;;
+    t)  TIME_OUT=$OPTARG
+        ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+[ "$1" = "--" ] && shift
+
+echo "Configurations..."
+echo
+echo "NAME=$NAME"
+echo "DJANGODIR=$DJANGODIR"
+echo "PORT=$PORT"
+echo "USER=$USER"
+echo "GROUP=$GROUP"
+echo "NUM_WORKERS=$NUM_WORKERS"
+echo "DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE"
+echo "TIME_OUT=$TIME_OUT" 
+echo 
+
+###### End Get command line options ###### 
+
+
+DJANGO_SETTINGS_MODULE=settings             # which settings file should Django use
+GUNICORN_ACCESS_LOG=$DJANGODIR/logs/gunicorn-access.log
+GUNICORN_ERROR_LOG=$DJANGODIR/logs/gunicorn-error.log
 echo "Starting $NAME as `whoami`"
  
 # Activate the virtual environment

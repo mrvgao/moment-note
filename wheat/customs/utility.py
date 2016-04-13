c7dadf70dfdd288d81f748f85651342882e335d6
fortyMiles <mqgao@outlook.com>
2016-04-13 10:43:58 +0800

    fix get image size bug

--- wheat/customs/utility.py
+++ wheat/customs/utility.py
@@ -14,9 +14,10 @@ def get_image_size(http_site_url, image_src):
       width, height = im.size
       return width, height
   except Exception as e:
+      print e
       return "Pciture Unvalid", "Picture Unvalid"
 
-MAILI_URL = 'http://121.40.158.110'
+MAILI_URL = 'http://121.40.158.110/media/'
 get_image_from_maili = partial(get_image_size, MAILI_URL)
 
 
@@ -28,10 +29,7 @@ def get_image_from_maili_by_img_list(images):
 
 if __name__ == '__main__':
     images = [
-          "/media/origin/22674a8d4f9055ef_uYYJJPK.JPEG",
-          "/media/origin/-41136f1eb1c62c15_zuwXuTa.JPEG",
-          "/media/origin/-43d2394e4465ba5a_TrOb7lB.JPEG",
-          "/media/origin/-2993ed61b26443bd_VcdY0vC.JPEG",
+          "origin/377743001"
     ]
 
     print(get_image_from_maili_by_img_list(images))

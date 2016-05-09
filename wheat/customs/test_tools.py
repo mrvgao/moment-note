90dfaf6062b2f4f734f140cecdcb96a046e9e756
fortyMiles <mqgao@outlook.com>
2016-05-09 11:59:18 +0800

    add a auto login to test

---
+++ wheat/customs/test_tools.py
@@ -0,0 +1,25 @@
+'''
+Utils for test.
+
+@Author Minchiuan <2016-May-9>
+'''
+
+from settings import API_VERSION
+URL_PREFIX = '/api/%s/' % API_VERSION
+
+
+def login_client(client, user_phone, password):
+    url = URL_PREFIX + 'users/login/'
+    post_data = {
+        'phone': user_phone,
+        'password': password
+    }
+    response = client.post(url, post_data)
+    return response
+
+
+def refresh_token(client, user_phone, password):
+    response = login_client(client, user_phone, password)
+    user_token = response.data['data']['token']['token']
+    client.credentials(HTTP_AUTHORIZATION='Token ' + user_token)
+    return user_token

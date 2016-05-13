1bd9c23728d1218b6cdd3142f95fbd996f5853f6
fortyMiles <mqgao@outlook.com>
2016-05-13 11:30:38 +0800

    change partial to function return

--- wheat/customs/request_tools.py
+++ wheat/customs/request_tools.py
@@ -2,21 +2,22 @@
 Some utils from request.
 '''
 
-import functools
 from errors import codes, exceptions
 from rest_framework import status
 
 
 def post_data_check(required_args):
     def check(func):
-        wrap = functools.partial(_check, func, required_args)
+        wrap = _check(func, required_args)
         return wrap
     return check
         
 
-def _check(func, required_args, viewset, request, *args, **kwargs):
-    for args in required_args:
-        if args not in request.data:
-            raise exceptions.APIError(code=codes.LACK_REQUIRED_PARAM, status=status.HTTP_400_BAD_REQUEST)
-    else:
-        return func(viewset, request, *args, **kwargs)
+def _check(func, required_args):
+    def wrap(viewset, request, *arg, **kwargs):
+        for args in required_args:
+            if args not in request.data:
+                raise exceptions.APIError(code=codes.LACK_REQUIRED_PARAM, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return func(viewset, request, *args, **kwargs)
+    return wrap

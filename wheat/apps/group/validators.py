8e4e4231968633cd1cc0ff0eb22ff6a2fd3aa7d0
fortyMiles <mqgao@outlook.com>
2016-05-13 15:52:54 +0800

    Fix check request for invitation.

--- wheat/apps/group/validators.py
+++ wheat/apps/group/validators.py
@@ -5,7 +5,7 @@ import functools
 from rest_framework import status
 
 from errors import codes, exceptions
-from .services import GroupService
+from .services import InvitationService
 
 
 def check_request(target='invitation'):
@@ -15,7 +15,7 @@ def check_request(target='invitation'):
         def wrapper(self, request, *args, **kwargs):
             if target == 'invitation':
                 id = kwargs.get('id')
-                invitation = GroupService.get_invitation(id=id)
+                invitation = InvitationService().get(id=id)
                 if not invitation:
                     raise exceptions.APIError(code=codes.NOT_EXIST, status=status.HTTP_404_NOT_FOUND)
                 else:

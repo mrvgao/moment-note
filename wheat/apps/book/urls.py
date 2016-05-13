7cf22138260738ddbda930d934e58a563de6961d
fortyMiles <mqgao@outlook.com>
2016-05-13 11:44:52 +0800

    Delete order viewset from book.

--- wheat/apps/book/urls.py
+++ wheat/apps/book/urls.py
@@ -3,6 +3,6 @@ from customs.urls import get_urlpattern
 
 urlpatterns = get_urlpattern({
     'author': apis.AuthorViewSet,
-    'order': apis.OrderViewSet,
+#    'order': apis.OrderViewSet,
     'book': apis.BookViewSet,
 }, api_name='book-api')
